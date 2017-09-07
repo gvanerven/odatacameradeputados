a4362b897fe1fc5422523629e5ff8db0798ac85b
Gustavo van Erven <gvanerven@gmail.com>
2017-09-07 19:12:57 -0300

    Inclusão dos parâmetros de ids, pagina, ordem, ordernadoPor

--- odatacameradeputados/proposicoes.py
+++ odatacameradeputados/proposicoes.py
@@ -2,13 +2,34 @@ import requests
 from . import defaults
 
 
-def get_proposicoes(debug=False):
+def get_proposicoes(debug=False, url=None, ids=None, pagina="1", ordem="ASC", ordenarPor="id"):
+    #?id=12665&id=12666&ordem=ASC&ordenarPor=id
     proposicoes = {}
-    r = requests.get(defaults.URL_BASE_PROPOSICOES.format(""), headers=defaults.HEADERS)
+    options = "?ordem=" + ordem + "&ordenarPor=" + ordenarPor + "&pagina=" + str(pagina)
+    if url == None:
+        url = defaults.URL_BASE_PROPOSICOES
+
+    if ids != None:
+        if isinstance(ids, list):
+            for id in ids:
+                options = options + "&id=" + str(id)
+        else:
+            options = options + "&id=" + str(ids)
+
+    r = requests.get(url.format(options), headers=defaults.HEADERS)
     if debug:
         print(r.content)
-    proposicoes["status_code"] = r.status_code
+
     proposicoes["proposicoes"] = r.json()
+    if "dados" in proposicoes["proposicoes"]:
+        proposicoes["total_retornado"] = len(proposicoes["proposicoes"]["dados"])
+    else:
+        proposicoes["total_retornado"] = -1
+
+    proposicoes["link"] = r.headers["link"]
+    proposicoes["total_count"] = r.headers["x-total-count"]
+    proposicoes["status_code"] = r.status_code
+
     if debug:
         print(proposicoes)
     return proposicoes
--- tests/test_get_proposicoes.py
+++ tests/test_get_proposicoes.py
@@ -3,13 +3,36 @@ import odatacameradeputados
 
 
 class GetInfoCDProposicoes(unittest.TestCase):
-    def test_get_proposicoes_simples(self):
+    def test_get_proposicoes_200OK(self):
+        proposicoes = odatacameradeputados.get_proposicoes()
+        self.assertTrue(proposicoes["status_code"] == 200)
+
+    def test_get_proposicoes_not_none(self):
         proposicoes = odatacameradeputados.get_proposicoes()
         self.assertNotEqual(proposicoes, None)
-        self.assertTrue(len(proposicoes) > 0)
-        self.assertTrue(proposicoes["status_code"] == 200)
+
+    def test_get_proposicoes_not_empty(self):
+        proposicoes = odatacameradeputados.get_proposicoes()
         self.assertTrue(len(proposicoes["proposicoes"]) > 0)
 
+    def test_get_proposicoes_any_id(self):
+        proposicoes = odatacameradeputados.get_proposicoes()
+        self.assertTrue("id" in proposicoes["proposicoes"]["dados"][0]
+                        and proposicoes["proposicoes"]["dados"][0]["id"] > 0)
+
+    def test_get_proposicoes_first_id(self):
+        # Preparing test
+        proposicoes = odatacameradeputados.get_proposicoes()
+        # retrieve the first id only
+        id_proposicoes = proposicoes["proposicoes"]["dados"][1]["id"]
+        proposicoes = odatacameradeputados.get_proposicoes(ids=id_proposicoes)
+        self.assertTrue("id" in proposicoes["proposicoes"]["dados"][0]
+                        and proposicoes["proposicoes"]["dados"][0]["id"] == id_proposicoes)
+
+    def test_get_proposicoes_200OK_pg2(self):
+        proposicoes = odatacameradeputados.get_proposicoes(pagina=2)
+        self.assertTrue(proposicoes["status_code"] == 200)
+        self.assertTrue(proposicoes["total_retornado"] > 0)
 
 if __name__ == '__main__':
     unittest.main()
